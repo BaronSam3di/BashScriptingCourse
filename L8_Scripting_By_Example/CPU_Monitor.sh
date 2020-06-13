7c61de0378b8be754cadec117f9add8f9fc785f5
geoffrey-owden <geoffrey.owden@ibm.com>
2020-06-13 13:35:40 +0100

    CPU Usage monitor

--- L8_Scripting_By_Example/CPU_Monitor.sh
+++ L8_Scripting_By_Example/CPU_Monitor.sh
@@ -4,23 +4,36 @@
 INTERVAL=$1
 while true $INTERVAL
 do
-    sleep 3
-    USAGE=`ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1 | awk '{ print $1}'`
-    USAGE=${USAGE%.*}
-    PID=`ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1 | awk '{ print $2}'`
-    PNAME=`ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1 | awk '{ print $3}'`
-    if [$USAGE -gt 80]
+    VALUE=$(ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1)
+    
+    USAGE=$(echo $VALUE | awk '{ print $1}')
+    USAGE1=${USAGE%.*}
+    PID1=$(echo $VALUE | awk '{ print $2}')
+    PNAME1=$(echo $VALUE | awk '{ print $3}')
+    
+#####
+# Debugging information 
+
+echo USAGE1 is set to $USAGE1
+echo PID1 is set to $PID1
+echo PNAME1 is set to $PNAME1
+# read
+
+    if [ $USAGE -gt 80 ]
     then
         USAGE1=$USAGE
         PID1=$PID
         PNAME=$PNAME
+        
         sleep 7
+        
         USAGE2=`ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1 | awk '{ print $1}'`
         USAG2E=${USAGE%.*}
         PID2=`ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1 | awk '{ print $2}'`
         PNAME2=`ps -eo pcpu,pid -o comm= | sort -k1 -n -r | head -1 | awk '{ print $3}'`
         [ $USAGE2 -gt 80] && [$PID1 = $PID2] && \
             mail -s 'CPU load of $PNAME is above *)%' root@blah.com < .
+    
     fi
         
    
